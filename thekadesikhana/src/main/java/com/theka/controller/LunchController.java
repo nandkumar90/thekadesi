08c997454abfb77f89b56e3b05fabe263dead64d
nand <nandkumar90@gmail.com>
2016-07-30 16:28:23 +0530

    lunch controller

--- thekadesikhana/src/main/java/com/theka/controller/LunchController.java
+++ thekadesikhana/src/main/java/com/theka/controller/LunchController.java
@@ -1,17 +1,39 @@
 package com.theka.controller;
 
+import java.util.LinkedList;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.google.gson.Gson;
+import com.theka.model.Menu;
+
 @RestController
 public class LunchController {
 	
-	@RequestMapping(value="/lunch/menu", method = RequestMethod.GET)
+	@RequestMapping(value="/lunch/menu", method = RequestMethod.GET,produces={"application/json"})
 	public @ResponseBody String getmenu() {
-		
-		return "item1: Rice	Rajma,	item2: Paneer Chilly, item3: 	Aloo Jeera,item4: 	paratha,item5: 	Papad (Lijjat), item 6: 	Salad,item7: 	Achar,	item8:Ghee,	item9: Fruit,item10:  Chaat,item11: Sweet";
-	}
+		Gson gson = new Gson();
+		Menu menu=new Menu();
+		LinkedList<String> items=new LinkedList<String>();
+		items.add("Rice	Rajma");
+		items.add("Paneer Chilly");
+		items.add("Aloo Jeera");
+		items.add("paratha");
+		items.add("Papad");
+		items.add("Salad");
+		items.add("Achar");
+		items.add("Ghee");
+		items.add("Fruit");
+		items.add("Chaat");
+		items.add("Sweet");
+		menu.setItems(items);
+		String me=gson.toJson(menu);
+
+        System.out.println(me);
 	
+		return me;
+	}
 }
