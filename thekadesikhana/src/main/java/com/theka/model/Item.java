8187a87839ede86b282a1422a9c6149a9f48b85e
nand <nandkumar90@gmail.com>
2016-07-30 16:26:44 +0530

    added restcontroller

---
+++ thekadesikhana/src/main/java/com/theka/model/Item.java
@@ -0,0 +1,20 @@
+package com.theka.model;
+
+public class Item {
+	private String name;
+	private String description;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+
+}
---
+++ thekadesikhana/src/main/java/com/theka/model/Menu.java
@@ -0,0 +1,21 @@
+package com.theka.model;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Menu {
+	
+	
+	private List<String> items=new LinkedList<String>();
+	
+
+	
+	
+	
+	public List<String> getItems() {
+		return items;
+	}
+
+	public void setItems(List<String> items) {
+		this.items = items;
+	}}
