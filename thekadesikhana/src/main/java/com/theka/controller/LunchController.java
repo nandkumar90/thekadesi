0505c85ed2a34dd98401ed2c7c85f01fc0075e3f
nand <nandkumar90@gmail.com>
2016-07-31 21:03:07 +0530

    update in method regarding lunch booking

--- thekadesikhana/src/main/java/com/theka/controller/LunchController.java
+++ thekadesikhana/src/main/java/com/theka/controller/LunchController.java
@@ -16,12 +16,12 @@ import com.theka.model.Menu;
 
 @RestController
 public class LunchController {
-	
-	@RequestMapping(value="/lunch/menu", method = RequestMethod.GET,produces={"application/json"})
+
+	@RequestMapping(value = "/lunch/menu", method = RequestMethod.GET, produces = { "application/json" })
 	public @ResponseBody String getmenu() {
 		Gson gson = new Gson();
-		Menu menu=new Menu();
-		LinkedList<String> items=new LinkedList<String>();
+		Menu menu = new Menu();
+		LinkedList<String> items = new LinkedList<String>();
 		items.add("Rice	Rajma");
 		items.add("Paneer Chilly");
 		items.add("Aloo Jeera");
@@ -34,36 +34,52 @@ public class LunchController {
 		items.add("Chaat");
 		items.add("Sweet");
 		menu.setItems(items);
-		String me=gson.toJson(menu);
+		String me = gson.toJson(menu);
 
-        System.out.println(me);
-	
+		System.out.println(me);
+
 		return me;
 	}
 
-
-	//for taking lunch booking parameters as form urm parameter
-	@RequestMapping(value="/lunch/booking", method = RequestMethod.POST,produces={"application/json"})
-	public @ResponseBody String lunchBooking(@RequestParam(value="email")String email, @RequestParam(value="phoneno")String phoneno,@RequestParam(value="nooflunchbox")int nooflunchbox,@RequestParam(value="location")int location, @RequestParam(value="deliveryTime")String deliveryTime) {
+	// for taking lunch booking parameters as form urm parameter
+	@RequestMapping(value = "/lunch/booking", method = RequestMethod.POST, produces = { "application/json" })
+	public @ResponseBody String lunchBooking(
+			@RequestParam(value = "email") String email,
+			@RequestParam(value = "phoneno") String phoneno,
+			@RequestParam(value = "nooflunchbox") int nooflunchbox,
+			@RequestParam(value = "location") String location,
+			@RequestParam(value = "deliveryTime") String deliveryTime) {
 		Gson gson = new Gson();
-		String test="email " + email+"  "+"phoneno  "+phoneno+"   "+"nooflunchbox   "+nooflunchbox+"   location"+location+"   deliveryTime "+deliveryTime;
-	
-		return test;
+//		String test = "email " + email + "  " + "phoneno  " + phoneno + "   "
+//				+ "nooflunchbox   " + nooflunchbox + "   location" + location
+//				+ "   deliveryTime " + deliveryTime;
+		LunchBooking lunchBooking = new LunchBooking();
+		lunchBooking.setDeliveryLocation(location);
+		lunchBooking.setDeliveryTime(deliveryTime);
+		lunchBooking.setPhoneNo(phoneno);
+		lunchBooking.setEmailId(email);
+		int orderId = (int) Math.random();
+		lunchBooking.setOrderId(orderId);
+		String booking = gson.toJson(lunchBooking);
+
+		return booking;
 	}
-	
-	
-	//for taking lunch booking parametes as a Lunchbooking object
-		@RequestMapping(value="/lunch/delivery", method = RequestMethod.POST,produces={"application/json"})
-		public @ResponseBody String lunchDelivery(LunchBooking lunchBooking) {
-			//extract 	expectedDeliveryDate, excpectedDeliveryTime from  deliverytime and set using setter;
-			Gson gson = new Gson();
-			String test="email " + lunchBooking.getEmailId()+"  "+"phoneno  "+lunchBooking.getPhoneNo()+"   "+"nooflunchbox   "+lunchBooking.getNoofTiffin()+"   location"+lunchBooking.getDeliveryLocation()+"  deliverytime "+lunchBooking.getDeliveryTime();
-		
-			return test;
-		} 
-	
-	
-	
-	
-	
+
+	// for taking lunch booking parametes as a Lunchbooking object
+	@RequestMapping(value = "/lunch/delivery", method = RequestMethod.POST, produces = { "application/json" })
+	public @ResponseBody String lunchDelivery(LunchBooking lunchBooking) {
+		// extract expectedDeliveryDate, excpectedDeliveryTime from deliverytime
+		// and set using setter;
+		Gson gson = new Gson();
+//		String test = "email " + lunchBooking.getEmailId() + "  " + "phoneno  "
+//				+ lunchBooking.getPhoneNo() + "   " + "nooflunchbox   "
+//				+ lunchBooking.getNoofTiffin() + "   location"
+//				+ lunchBooking.getDeliveryLocation() + "  deliverytime "
+//				+ lunchBooking.getDeliveryTime();
+		int orderId = (int) Math.random();
+		lunchBooking.setOrderId(orderId);
+		String Booking = gson.toJson(lunchBooking);
+		return Booking.toString();
+	}
+
 }
