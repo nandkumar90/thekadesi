901308068ae73945bfe67163e5f8b63548b2e8b7
nand <nandkumar90@gmail.com>
2016-07-31 20:37:56 +0530

    added controller for lunch tet booking

--- thekadesikhana/src/main/java/com/theka/controller/LunchController.java
+++ thekadesikhana/src/main/java/com/theka/controller/LunchController.java
@@ -1,13 +1,17 @@
 package com.theka.controller;
 
+import java.sql.Time;
+import java.util.Date;
 import java.util.LinkedList;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.google.gson.Gson;
+import com.theka.model.LunchBooking;
 import com.theka.model.Menu;
 
 @RestController
@@ -36,4 +40,29 @@ public class LunchController {
 	
 		return me;
 	}
+
+
+	//for taking lunch booking parameters as form urm parameter
+	@RequestMapping(value="/lunch/booking", method = RequestMethod.POST,produces={"application/json"})
+	public @ResponseBody String lunchBooking(@RequestParam(value="email")String email, @RequestParam(value="phoneno")String phoneno,@RequestParam(value="nooflunchbox")int nooflunchbox,@RequestParam(value="location")int location, @RequestParam(value="deliveryTime")String deliveryTime) {
+		Gson gson = new Gson();
+		String test="email " + email+"  "+"phoneno  "+phoneno+"   "+"nooflunchbox   "+nooflunchbox+"   location"+location+"   deliveryTime "+deliveryTime;
+	
+		return test;
+	}
+	
+	
+	//for taking lunch booking parametes as a Lunchbooking object
+		@RequestMapping(value="/lunch/delivery", method = RequestMethod.POST,produces={"application/json"})
+		public @ResponseBody String lunchDelivery(LunchBooking lunchBooking) {
+			Gson gson = new Gson();
+			String test="email " + lunchBooking.getEmailId()+"  "+"phoneno  "+lunchBooking.getPhoneNo()+"   "+"nooflunchbox   "+lunchBooking.getNoofTiffin()+"   location"+lunchBooking.getDeliveryLocation()+"   expected_deliverytime "+lunchBooking.getDeliveryTime();
+		
+			return test;
+		} 
+	
+	
+	
+	
+	
 }
